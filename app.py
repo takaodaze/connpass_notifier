3ef55522df5c604c3fe164d709cf018223cd6025
gin.2525 <gin.2525@linecorp.com>
2020-01-24 22:31:50 +0900

    prepure

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
+import flask
 import requests
-from bs4 import BeautifulSoup
- 
-r = requests.get("https://connpass.com/calendar/fukuoka/")
- 
-soup = BeautifulSoup(r.content, "html.parser")
- 
-# ニュース一覧を抽出
-event_list = soup.select('a.Help')
-for event in event_list:
-	print(event.gw)
+from bs4 import BeautifulStoneSoup
+import scrayper
+import line
+
+app = flask(__name__)
+
+@app.route('/')
+def index():
+	return '元気に動作中'
+
+@app.route('/callback')
+def callback():
+	    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # https://developers.line.biz/console/channel/1653365219/basic/
+    # LINEコンソールのwebhook URL のエラー回避用.
+    if event.reply_token == "00000000000000000000000000000000":
+        return
+
+	if evnet.message.text == 'ふくおか':
+		f_date = datetime.date(2020,1,1)
+		t_date = datetime.date(2020, 1, 31)
+		result = scrayper.get_connpass('fukuoka', 1, f_datem, t_date)
+		line_bot_api.reply_message(
+			event.reply_token, handle_message=result
+		)
+		
+
+
+	
--- scrayper.py
+++ scrayper.py
@@ -1,1 +1,30 @@
-def scrape()
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import time
+
+def get_connpass(prefectures, page, from_date, to_date):
+	result_texts=[]
+	page += 1
+	previours_results = False
+	for i in range(1,page):
+		url = f'https://connpass.com/search/?page={i}&q=&start_from={from_date.year}%2F{from_date.month}%2F{from_date.day}&start_to={to_date.year}%2F{to_date.month}%2F{to_date.day}&prefectures={prefectures}&selectItem={prefectures}'
+		r = requests.get(url)
+		soup = BeautifulSoup(r.content, "html.parser")
+		results = soup.select('a.url.summary')
+		if previours_results == results and i!=1:
+			return result_texts
+		for event in results:
+			result_texts.append(event.text)
+		time.sleep(0.8)
+		previours_results = results
+	return result_texts
+	
+
+if __name__ == "__main__":
+	f_date = datetime.date(2020,1,1)
+	t_date = datetime.date(2020, 1, 31)
+	result_texts = get_connpass('fukuoka', 40, f_date, t_date)
+	for event in result_texts:
+		print(event)
+
