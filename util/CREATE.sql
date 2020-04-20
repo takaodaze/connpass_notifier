f4c722cf2958b0478de0b9783b4b2f24fe2c6f2d
gin.2525 <gin.2525@linecorp.com>
2020-04-20 14:09:21 +0900

    rm trash

--- util/CREATE.sql
+++ util/CREATE.sql
@@ -1,4 +1,5 @@
-CREATE TABLE events(
+CREATE TABLE events
+(
 	event_name varchar(100),
 	event_date date,
 	event_url varchar(300),
@@ -7,15 +8,8 @@ CREATE TABLE events(
 	domain varchar(20)
 );
 
-CREATE TABLE users(
+CREATE TABLE users
+(
 	user_id VARCHAR(255),
 	display_name VARCHAR(30)
 );
-
-
-SELECT event_name,event_date,insert_date
-        FROM events
-        WHERE insert_date BETWEEN '2020-02-01' AND '2020-02-05'
-        AND event_date >= '2020-02-05'
-        ORDER BY event_date ASC
-        LIMIT 50
